d4f52c7a38735995ab5787b3fb369e516884944f
Amit Kumar <83582726+amitk221@users.noreply.github.com>
2022-08-09 13:39:59 +0530

    Add files via upload

---
+++ build/precache-manifest.df8d564a2dc615f028bfe7fc3ba7e4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9046974fe269ac83ae49b550e1750bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75dfa5efbdc75a6636b2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ffe5e40a34b0f33cb43",
+    "url": "/static/js/2.12b29ee6.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.12b29ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dfa5efbdc75a6636b2",
+    "url": "/static/js/main.18dd7290.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
